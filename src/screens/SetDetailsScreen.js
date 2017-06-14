09b746993ff7ff3f8180583dfa5dd56546c4de1b
Magnus Holm <mail@magnusholm.com>
2017-06-14 11:56:14 +0100

    create SetDetailsScreen

--- src/screens/SetDetailsScreen.js
+++ src/screens/SetDetailsScreen.js
@@ -1,1 +1,26 @@
+import React, { Component } from 'react';
+import { TouchableHighlight, Text, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import SetList from '../components/SetList';
 
+class SetDetailsScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.viewStyle}>
+        <SetDetailsList />
+      </View>
+    );
+  }
+}
+
+const styles = {
+  viewStyle: {
+    marginTop: 60
+  }
+}
+
+export default SetListScreen;
